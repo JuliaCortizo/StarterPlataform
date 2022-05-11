3f3b88c049530ab652de7633f2ebd8ae8f090b74
mnia <marcos.ferreira@gft.com>
2022-05-11 15:06:09 -0300

    adicionando arquivos iniciais

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StarterPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
